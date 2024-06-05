4b682acd63be31e15ffc65fc645fef101835b791
Wilson PC <wilsonsamueli33@gmail.com>
2024-06-05 22:53:42 +0100

    'send'

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from "react-router-dom";
+import { auth } from "@/firebase-config";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "./Logo";
 
 const Footer = () => {
@@ -12,15 +13,35 @@ const Footer = () => {
             Power Up Your Life: The Latest in Laptops, Gadgets, and Software
           </p>
           <div className="mt-5 flex flex-col gap-4 md:mt-12">
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/login">Login</NavLink>
-            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/about" className="text-base font-medium">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="text-base font-medium">
+              About
+            </NavLink>
+            {(auth.currentUser?.uid && !auth.currentUser.isAnonymous && (
+              <NavLink to="/user/carts" className="text-base font-medium">
+                My Account
+              </NavLink>
+            )) || (
+              <>
+                <NavLink to="/login" className="text-base font-medium">
+                  Login
+                </NavLink>
+                <NavLink to="/register" className="text-base font-medium">
+                  Register
+                </NavLink>
+              </>
+            )}
           </div>
         </div>
       </div>
 
       <div className="mt-8">
-        <div className="inline-flex items-center justify-center gap-1 w-full">
+        <Link
+          to="https://app.imperial.learnchameleon.com/"
+          className="inline-flex items-center justify-center gap-1 w-full"
+        >
           <div>
             <span className="text-xs font-bold">Powered By</span>
           </div>
@@ -31,8 +52,10 @@ const Footer = () => {
                 "url('https://lh6.googleusercontent.com/4YxlDkJwbdpqJk7dW0YggqzmSoOWDm-E4_sGqwa18jVKfIzvv9IxTvtkXv9on3JQMV1JvPpvU5R1u5nYGBKwx-PKq_vPr04ada4GYNVet1rx5BWGdLb2Nca4pvWZwvp72A=w976')",
             }}
           ></div>
-        </div>
-        <p className="text-center text-xs">&copy;&nbsp;Copyright {new Date().getFullYear()}</p>
+        </Link>
+        <p className="text-center text-xs">
+          &copy;&nbsp;Copyright {new Date().getFullYear()}
+        </p>
       </div>
     </div>
   );
