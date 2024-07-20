cf5b3593ea550b57b9869b7320822db4962254a9
Wilson PC <wilsonsamueli33@gmail.com>
2024-07-20 16:07:16 +0100

    app:handling sync

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -18,6 +18,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+// connectAuthEmulator(auth, "http://127.0.0.1:9099");
+// connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
 
 export { app, auth, firestore };
 
