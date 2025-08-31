33c8d651a18654b63b251310434cff70ce7d9234
Alpha <alphasoft2021@gmail.com>
2025-08-31 22:19:16 +0100

    fix: update logo alt text and page title to include full branding "Emeralds Digital Venture"

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -26,7 +26,7 @@ const MainLayout: React.FC<{
       )}
 
       <meta name="description" content={description} />
-      <title>{title} - Emeralds Venture</title>
+      <title>{title} - Emeralds Digital Venture</title>
       <div>
         {/* <div className="inline-flex flex-col justify-between w-full min-h-screen "> */}
         {!no_navbar && <Navbar />}
