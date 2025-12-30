dae66f0a57b45da3bbc56cc7835e2b17eb0bebe8
Alpha <alphasoft2021@gmail.com>
2025-12-30 11:01:47 +0100

    feat: update PaymentPage to include support contact link

--- src/Pages/PaymentPage.tsx
+++ src/Pages/PaymentPage.tsx
@@ -8,7 +8,7 @@ import _ from "lodash";
 import moment from "moment";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { usePaystackPayment } from "react-paystack";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 interface MetadataInterface {
     [key: string]: any;
@@ -230,10 +230,10 @@ const PaymentPage = () => {
                             <input type="text" placeholder="Enter email address" className="input-checkout" value={data?._m?.email} disabled name="email" />
                             <div className="text-base lg:text-lg font-medium">
                                 <span>If You Need more Explanation on {_.upperFirst(data?._m?.appName)}, or You have Any Issue with Making Payment, contact support by clicking on the below button</span>
-                                <button className="inline-flex gap-0.5 items-center bg-red-500/35 underline underline-offset-2 text-sm py-1 text-gray-800 font-semibold px-2 rounded-md"> <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <Link target="_blank" to={decodeURIComponent(data?._m?.support).replace("{{message}}", encodeURIComponent(`Hello Agent, I have issue with making the final payment process for *${_.upperFirst(data?._m?.appName)} ${data?._m?.activePlanTitle}(NGN${data?._m?.amount})*. How can your help me out here?`))} className="inline-flex gap-0.5 items-center bg-red-500/35 underline underline-offset-2 text-sm py-1 text-gray-800 font-semibold px-2 rounded-md"> <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fillRule="evenodd" d="M12 2a7 7 0 0 0-7 7 3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1a1 1 0 0 0 1-1V9a5 5 0 1 1 10 0v7.083A2.919 2.919 0 0 1 14.083 19H14a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h1a2 2 0 0 0 1.732-1h.351a4.917 4.917 0 0 0 4.83-4H19a3 3 0 0 0 3-3v-2a3 3 0 0 0-3-3 7 7 0 0 0-7-7Zm1.45 3.275a4 4 0 0 0-4.352.976 1 1 0 0 0 1.452 1.376 2.001 2.001 0 0 1 2.836-.067 1 1 0 1 0 1.386-1.442 4 4 0 0 0-1.321-.843Z" clipRule="evenodd" />
                                 </svg>
-                                    Contact Support</button>
+                                    Contact Support</Link>
                             </div>
                             <div className="flex justify-end mt-8">
                                 <button onClick={() => {
